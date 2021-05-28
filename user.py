aaebb78558bfc5382630b2c5a29e47b96888cb66
Fai <chepngetichfaith46@gmail.com>
2021-05-28 23:02:19 +0300

    Add a method to save user name and passwords

--- user.py
+++ user.py
@@ -8,5 +8,11 @@ class User:
     def __init__(self,username,password):
         self.username = username
         self.password = password
+        
+    def save_user(self):
+        '''
+        save_user method saves a new user objects to the user list
+        '''
+        User.user_list.append(self)
        
     
