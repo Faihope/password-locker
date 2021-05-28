94e84e82e6fe040e3847381f917829dc9d6dd769
Fai <chepngetichfaith46@gmail.com>
2021-05-28 22:57:44 +0300

    Add empty array to hold user data and an empty array

--- user.py
+++ user.py
@@ -3,3 +3,10 @@ class User:
     class that generates new instace of user
     '''
     
+    user_list = []
+    
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+       
+    
