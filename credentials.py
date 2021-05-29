0b31a470008b6a30a04a7cdcdcd8c77946b8fad6
Fai <chepngetichfaith46@gmail.com>
2021-05-29 09:17:14 +0300

    Add class credentials to creatse instance of credentials

---
+++ credentials.py
@@ -0,0 +1,21 @@
+from user import User
+
+
+class Credentials:
+    '''
+    A class to create instance of credentials
+    '''
+    
+    user_credentials = []
+    
+def __init__(self,save_username,save_password):
+    self.save_username = ""
+    self.save_password = ""
+    
+    
+def save_credentials(self):
+    '''
+    saves new credentials of the user
+    '''
+    
+    User.user_credentials.append(self)
