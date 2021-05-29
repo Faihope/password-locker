8611d9691682730fc8fa0769f685a0b27d0e8992
Fai <chepngetichfaith46@gmail.com>
2021-05-29 10:10:22 +0300

    Add test case to check if the objects aer initialized properly

--- user-test.py
+++ user-test.py
@@ -8,6 +8,26 @@ class TestUser(unittest.TestCase):
          unnitest.Testcase:Testcase class that helps in creating test cases
     '''
     
+    def setUp(self):
+        '''
+        Set up method to run before a test case
+        '''
+        
+        self.new_user = User('faith','1234')
+        
+    def test_save_user(self):
+        
+        '''
+        test case to test if the object is initialized properly
+        '''
+        
+        self.assertEqual(self.new_user.username,'faith')
+        self.assertEqual(self.new_user,'password')
+        
+        
+        
+        
+    
 
     
     
